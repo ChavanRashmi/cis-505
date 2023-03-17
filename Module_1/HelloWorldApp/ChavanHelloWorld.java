bbf69c72a5f3af3d79eab5ae24d9fbabd18ac93c
ChavanRashmi <rchavan@my365.bellevue.edu>
2023-03-17 17:39:39 -0500

    CIS505 || Assignment 1.2 || Rashmi Chavan

---
+++ Module_1/HelloWorldApp/ChavanHelloWorld.java
@@ -0,0 +1,18 @@
+/*
+    Program: Create a program that output’s the String value “Hello World from YourFirstName YourLastName"
+    Author: Rashmi Chavan
+    Class: CIS505
+ */
+
+// Class to print out the text - Hello World from YourFirstName YourLastName
+public class ChavanHelloWorld {
+
+    public static void main(String[] args) {
+        final String text = "Hello World ";
+        final String firstName = "Rashmi ";
+        final String lastName = "Chavan";
+
+        System.out.println(text+firstName+lastName);
+    }
+
+}
