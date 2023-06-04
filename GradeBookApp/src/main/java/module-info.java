b021fa279f118ecfc57d92108a2a028021dfef41
ChavanRashmi <rchavan@my365.bellevue.edu>
2023-06-03 20:22:47 -0500

    CIS505 || Assignment 12 || Capstone Project | Rashmi Chavan

--- GradeBookApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.gradebookapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                        
-    opens com.example.gradebookapp to javafx.fxml;
-    exports com.example.gradebookapp;
-}
